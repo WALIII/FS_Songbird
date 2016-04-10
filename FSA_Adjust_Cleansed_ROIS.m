d0bda9839828a110cab5023902571d9080add4ea
WALIII <bliberti@bu.edu>
2016-04-10 10:55:27 -0400

    update scripts

---
+++ FSA_Adjust_Cleansed_ROIS.m
@@ -0,0 +1,15 @@
+function ROI_data_cleansed = FSA_Adjust_Cleansed_ROIS(ROI_data_cleansed,Adjusted_Filenames)
+
+
+for iv = 1:5;
+for i = 1:size(ROI_data_cleansed{iv}.filename,2)
+   OG = cellstr(ROI_data_cleansed{iv}.filename{i}(1:end-4));
+    index = find(strcmp(Adjusted_Filenames{iv}{1}, OG));
+    
+    ROI_data_cleansed{iv}.filenames2{i} = Adjusted_Filenames{iv}{2}(index);
+end
+end
+
+
+    
+
---
+++ Motif Analysis/FSA_Adjust_MotifNo.m
@@ -0,0 +1,30 @@
+function [Adjusted_Filenames] = FSA_Adjust_MotifNo(sorted_syllable, filenames)
+
+counter3 = 1; % Placeholder for all data;
+for i = 1:size(filenames,2)
+counter = 1; % for old data, and placeholder
+counter2 = 1; % for new data
+   FN = filenames{i}(1:end-4);
+    for ii = 1:size(sorted_syllable{i},1)
+        place = [1:size(sorted_syllable{i},1)];
+
+if sorted_syllable{i}(ii) < (250*24400)/48000;
+    Old_FileName{counter3} = [];
+    New_FileName{counter3} = strcat(FN,'_000',num2str(counter2));
+    counter2 = counter2+1;
+    counter3 = counter3+1;
+else
+Old_FileName{counter3} = strcat(FN,'_000',num2str(counter));
+New_FileName{counter3} = strcat(FN,'_000',num2str(counter2));
+counter2 = counter2+1;
+counter = counter+1;
+counter3 = counter3+1;
+end
+    end
+    
+end
+
+Adjusted_Filenames{:,1} = Old_FileName;
+Adjusted_Filenames{:,2} = New_FileName;
+
+
