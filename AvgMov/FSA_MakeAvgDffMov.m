9a3046c035da9f3e59e912ae60d175a89cc3fd12
WALIII <bliberti@bu.edu>
2016-04-14 10:49:35 -0400

    update off scripts

--- AvgMov/FSA_MakeAvgDffMov.m
+++ AvgMov/FSA_MakeAvgDffMov.m
@@ -2,6 +2,13 @@ function AVG_MOV = FSA_MakeAvgDffMov()
 % Make average Dff movie
 
 
+
+filt_rad=20; % gauss filter radius
+filt_alpha=30; % gauss filter alpha
+lims=3; % contrast prctile limits (i.e. clipping limits lims 1-lims)
+cmap=colormap('jet');
+per=0; % baseline percentile (0 for min)
+
 mov_listing=dir(fullfile(pwd,'*.mat')); % Get all .mat file names
 mov_listing={mov_listing(:).name};
 filenames=mov_listing;
@@ -15,19 +22,36 @@ for  iii = 1:length(mov_listing)
 DispWrd = strcat('moving to: ', file);
 disp(DispWrd);
 
+% Extract video data:
+mov_data = mov_data_aligned;
+[rows,columns,frames]=size(mov_data);
 
-  mov_data = mov_data_aligned;
-
+% Reformat Movie data:
   for i=1:(length(mov_data)-2)
      mov_data3 = mov_data(i).cdata;
      mov_data4 = mov_data(i+1).cdata;
-     %mov_data5 = single(rgb2gray(mov_data(i+2).cdata));
      mov_data2(:,:,i) = (mov_data3 + mov_data4)/2;
   end
 
-mov_data2 = imresize((mov_data2),.25);
+test = imresize((mov_data2),.25);% Scale Data
 
-AggMov_data(:,:,:,iii) = mov_data2(:,:,1:50);
+%%%=============[ FILTER Data ]==============%%%
+
+disp('Gaussian filtering the movie data...');
+
+h=fspecial('gaussian',filt_rad,filt_alpha);
+test=imfilter(test,h,'circular');
+
+disp(['Converting to df/f using the ' num2str(per) ' percentile for the baseline...']);
+
+baseline=repmat(prctile(MOV_DATA,per,3),[1 1 frames]);
+
+dff=((MOV_DATA-baseline)./(baseline)).*100;
+
+%
+
+
+AggMov_data(:,:,:,iii) = dff(:,:,1:50);
 
 clear mov_data; clear mov_data2; clear mov_data3; clear mov_data4;
 end
