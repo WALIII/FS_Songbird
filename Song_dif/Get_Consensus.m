6e5f3d30a81e7e87f2e42cc7a17df14f404f32c9
WALIII <bliberti@bu.edu>
2016-05-17 17:53:58 -0400

    update get consensus

---
+++ Song_dif/Get_Consensus.m
@@ -0,0 +1,26 @@
+
+% Get_Consensus
+
+for DAY = 1:5;
+
+	[consensus f t]=acontour(A{DAY}.mic_data{1},24400);
+	% compute sdi, relevant quantities, store
+
+	ntrials=size(A{DAY}.mic_data,2);
+	[rows,columns]=size(consensus);
+
+	consensus=zeros(rows,columns,ntrials,'single');
+
+	parfor j=1:ntrials
+		disp([num2str(j) ' of ' num2str(ntrials)]);
+		[consensus(:,:,j)]=acontour(A{DAY}.mic_data{j},24400);
+	end
+
+	Gconsensus{DAY}=consensus;
+clear consensus;
+end
+
+consensus = Gconsensus;
+    
+
+
